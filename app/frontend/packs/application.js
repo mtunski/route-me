b8ef6e701304e1778267625513b7cbc6845e6065
Mateusz Tuński <mateusz@tunski.co>
2017-10-02 21:01:25 +0200

    HMR

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,14 @@ import { render } from 'react-dom'
 
 import MapPage from '../pages/MapPage'
 
-document.addEventListener('DOMContentLoaded', () =>
-  render(<MapPage />, document.querySelector('#app'))
-)
+document.addEventListener('DOMContentLoaded', () => {
+  const mountNode = document.querySelector('#app')
+
+  render(<MapPage />, mountNode)
+  if (module.hot) {
+    module.hot.accept('../pages/MapPage', () => {
+      const NextRootComponent = require('../pages/MapPage').default
+      render(<NextRootComponent />, mountNode)
+    })
+  }
+})
