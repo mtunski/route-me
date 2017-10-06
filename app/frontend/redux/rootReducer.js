2bb323a12ed1a12669c187f24f2821f8299d7e5f
Mateusz Tuński <mateusz@tunski.co>
2017-10-06 20:23:15 +0200

    Add redux-form

--- app/frontend/redux/rootReducer.js
+++ app/frontend/redux/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import locationsReducer from './reducers/locations'
 import routeReducer from './reducers/route'
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
   locations: locationsReducer,
   route: routeReducer,
+  form: formReducer,
 })
