d42699bcabd9a8510bad43cddcc0b438bada24eb
Mateusz Tuński <mateusz@tunski.co>
2017-10-08 22:37:11 +0200

    Nicer looking markers

--- app/frontend/pages/MapPage/components/Location.js
+++ app/frontend/pages/MapPage/components/Location.js
@@ -18,6 +18,16 @@ export default class Location extends PureComponent {
     <Marker
       defaultPosition={this.props.coordinates}
       onClick={this.handleClick}
+      icon={{
+        path: 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z',
+        anchor: new google.maps.Point(12, 24),
+        strokeColor: "#00796B",
+        strokeOpacity: 1,
+        strokeWeight: 0,
+        fillColor: '#00796B',
+        fillOpacity: 1,
+        scale: 2,
+      }}
       key={this.props.id}
     />
 }
